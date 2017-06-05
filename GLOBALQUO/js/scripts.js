fe8224c87e18fba43255d82005c2d44312c49690
CarlFdez <mefmunoz@ucm.es>
2017-06-05 21:11:30 +0200

    Global1200

--- GLOBALQUO/js/scripts.js
+++ GLOBALQUO/js/scripts.js
@@ -46,5 +46,32 @@ $(document).ready(function() {
 });
 /*final*/
 
+/* CONTACTO*/
+// Check if name has been entered
+
+if (!$_POST['name']) {
+    $errName = 'Introduce tu nombre.';
+}
+// Check if email has been entered and is valid
+
+if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    $errEmail = 'Introduce tu dirección de correo electrónico.';
+}
+//Check if message has been entered
+
+if (!$_POST['message']) {
+    $errMessage = 'Introduce tu consulta.';
+}
+
+
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage) {
+    if (mail ($to, $subject, $body, $from)) {
+        $result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+    } else {
+        $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+    }
+}
+/*final*/
 
 
