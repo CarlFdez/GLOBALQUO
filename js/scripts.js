0d45feffebe0337dda955a9278884f4a7cb5e14c
CarlFdez <mefmunoz@ucm.es>
2017-06-05 11:51:43 +0200

    Pág. Global

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+(function($) {
+    "use strict";
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 60
+    });
+
+    $('#topNav').affix({
+        offset: {
+            top: 200
+        }
+    });
+    
+    new WOW().init();
+    
+    $('a.page-scroll').bind('click', function(event) {
+        var $ele = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($ele.attr('href')).offset().top - 60)
+        }, 1450, 'easeInOutExpo');
+        event.preventDefault();
+    });
+    
+    $('.navbar-collapse ul li a').click(function() {
+        /* always close responsive nav after click */
+        $('.navbar-toggle:visible').click();
+    });
+
+    $('#galleryModal').on('show.bs.modal', function (e) {
+       $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
+    });
+
+})(jQuery);
+
+
+/*acordeon*/
+$(document).ready(function() {
+    $('.accordion').find('.accordion-toggle').click(function() {
+        $(this).next().slideToggle('600');
+        $(".accordion-content").not($(this).next()).slideUp('600');
+    });
+    $('.accordion-toggle').on('click', function() {
+        $(this).toggleClass('active').siblings().removeClass('active');
+    });
+});
+/*final*/
+
+/* CONTACTO*/
+// Check if name has been entered
+
+if (!$_POST['name']) {
+    $errName = 'Introduce tu nombre.';
+}
+// Check if email has been entered and is valid
+
+if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    $errEmail = 'Introduce tu dirección de correo electrónico.';
+}
+//Check if message has been entered
+
+if (!$_POST['message']) {
+    $errMessage = 'Introduce tu consulta.';
+}
+
+
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage) {
+    if (mail ($to, $subject, $body, $from)) {
+        $result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+    } else {
+        $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+    }
+}
+/*final*/
+
+
